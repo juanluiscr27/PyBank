522e917a799ef8a9b4cbe4bca1ec88c6566a74b4
Juan Luis <juanluiscr27@hotmail.com>
2022-07-21 19:33:07 -0400

    Main

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ CSD 4523 - Python II
 CSAM   Group 02   2022S
 """
 from db.database_conn import ConnectionPool
+import tkinter as tk
+
+from model.agent import Agent
+from ui.gui import *
 
 
 def main():
     """ Application main method """
     # Database Configuration
     config = {
-        'user': 'test_user',
-        'password': 'secret',
+        'user': 'pybank',
+        'password': 'Lambton2022S',
         'host': 'localhost',
-        'port': '3307',
+        'port': '3306',
         'database': 'pybank',
         'pool_name': 'pybank_conn_pool',
         'pool_size': 1
@@ -21,6 +25,15 @@ def main():
     print("* Welcome to PyBank *")
 
     ConnectionPool.create_pool(**config)
+
+    active_agent = Agent()
+    window = tk.Tk()
+    result = Return();
+
+    agent_login(window, active_agent, result)
+    #if return == 0:
+        #open search window
+    window.mainloop()
 
 
 # Start the program
