d53069bfe54da04304fb588004dc3901020330e0
hbeltrane <hbeltrane@hotmail.com>
2022-07-21 21:55:19 -0400

    Login window - Ok

--- ui/gui.py
+++ ui/gui.py
@@ -6,6 +6,7 @@ from tkinter import Menu
 
 def agent_login(window, active_agent, result):
     def action_login():
+        error_message.config(text="")
         if username.get() == "":
             error_message.config(text="Please input your username", foreground="Red")
         elif password.get() == "":
@@ -14,6 +15,9 @@ def agent_login(window, active_agent, result):
             active_agent.login(username.get(), password.get(), result)
             if result.code == "00":
                 print("Login OK")
+                # First instructions
+                area.destroy()
+                window.geometry("800x600")
             else:
                 error_message.config(text=result.message[1], foreground="Red")
 
@@ -23,14 +27,14 @@ def agent_login(window, active_agent, result):
     area = ttk.LabelFrame(window, text="Agent login")
     area.grid(column=0, row=0, padx=100, pady=100)
 
-    username_label = ttk.Label(area, text="username")
+    username_label = ttk.Label(area, text="Username")
     username_label.grid(column=0, row=0, padx=20, pady=10)
 
     username = tk.StringVar()
     username_text = ttk.Entry(area, width=30, textvariable=username)
     username_text.grid(column=1, row=0, padx=20, pady=10)
 
-    password_label = ttk.Label(area, text="password")
+    password_label = ttk.Label(area, text="Password")
     password_label.grid(column=0, row=1, padx=20, pady=10)
 
     password = tk.StringVar()
