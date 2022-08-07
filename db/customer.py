787714d5136beb8fb4c39aea4494259274dc51a1
Juan Luis <juanluiscr27@hotmail.com>
2022-08-06 21:23:45 -0400

    -m Search customer and Search accounts functions

--- db/customer.py
+++ db/customer.py
@@ -1,13 +1,56 @@
-from dataclasses import dataclass, field
+
+from mysql.connector import errors
+
+from db.database_conn import ConnectionPool
+from model.result import Return
 
 
-@dataclass(kw_only=True, slots=True)
 class CustomerModel:
-    customer_id: int
-    pin: str
-    first_name: str
-    last_name: str
-    address: str
-    phone_number: str
-    email: str
-    creation_date: str
+    conn: None
+
+    @classmethod
+    def create_customer(cls, active_agent, input_username: str, result: Return):
+        conn = None
+        # Request a database connection from the pool
+        try:
+            conn = ConnectionPool.get_connection()
+
+            if conn.is_connected():
+                # print("Connection successful")
+                query = "SELECT username, password, first_name, last_name, position_id" \
+                        "  FROM agents " \
+                        "  WHERE username = %(username)s"
+                cursor = conn.cursor()
+
+                # Query scape parameters
+                agent_username = {
+                    'username': input_username
+                }
+
+                cursor.execute(query, agent_username)
+                row = cursor.fetchone()
+                if cursor.rowcount > 0:
+
+                    # Unpack row fields from the result
+                    (username, password, first_name, last_name, position_id) = row
+
+                    active_agent.username = username
+                    active_agent.password = password
+                    active_agent.first_name = first_name
+                    active_agent.last_name = last_name
+                    active_agent.position_id = position_id
+
+                    result.set_code("00")
+                else:
+                    result.set_code("01")
+
+                cursor.close()
+
+        except errors.PoolError as pe:
+            print(f"{pe.errno} Pool is exhausted due to many connection requests")
+        except errors.Error as er:
+            conn.rollback()
+            print(f'{er.errno}: {er.msg}')
+        finally:
+            if conn.is_connected():
+                conn.close()
--- model/agent.py
+++ model/agent.py
@@ -12,9 +12,35 @@ class Agent:
     position_id: int = ""
 
     def login(self, username, password, result):
-
+        """ Search for the bank agent and validate his credentials """
         AgentModel.validate_agent(self, username, result)
 
         if result.code == "00":
             if self.password != password:
                 result.set_code("02")
+
+    @staticmethod
+    def search_customer(search_string, result):
+        """ Search bank customers who match de provided search string.
+            Return a list of customers who satisfy the search criteria. """
+        customers_result = []
+        AgentModel.search_customer(search_string, customers_result, result)
+        return customers_result
+
+    @staticmethod
+    def search_account(search_string, result):
+        """ Search bank accounts who match de provided search string.
+            Return a list of accounts who satisfy the search criteria. """
+        account_result = []
+        AgentModel.search_account(search_string, account_result, result)
+        return account_result
+
+    @staticmethod
+    def create_customer():
+        """ Create a new bank customer """
+        pass
+
+    @staticmethod
+    def open_customer():
+        """ Create a new bank account """
+        pass
