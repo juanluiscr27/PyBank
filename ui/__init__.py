439cc55f0bfa273a7406f3375f0e4195d42be8ab
hbeltrane <hbeltrane@hotmail.com>
2022-08-09 10:07:21 -0400

    Search window

--- ui/__init__.py
+++ ui/__init__.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox
+from datetime import datetime
 
 from model.agent import Agent
+from model.customer import Customer
 from model.result import Return
